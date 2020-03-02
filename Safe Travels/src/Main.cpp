843e8f8678c4a63432ac486f7509fcf478ae49e2
Justin <45080374+jro0100@users.noreply.github.com>
2020-03-01 20:31:50 -0500

    First Commit

---
+++ Safe Travels/src/Main.cpp
@@ -0,0 +1,14 @@
+//-----------------------------SAFE TRAVELS---------------------//
+
+//Game designed on 2/29/2020
+//Coding began on 3/1/2020
+
+#include <iostream>
+#include "Player.h"		  //Tested
+#include "FamilyMember.h" //Tested
+#include "Date.h"		  //Tested
+#include "Journey.h"	  //Tested
+
+int main()
+{
+}
